a45b616e50dbd81c795845c11d068dc0f48ef9dc
Joey Endicott <endicottj@weiserschools.org>
2019-06-05 13:47:24 -0600

    Modified

---
+++ public/index.php
@@ -0,0 +1,28 @@
+﻿<!DOCTYPE html>
+<html>
+	<head>
+		<title>Joey Endicott's CS 516 Project</title>
+		<link href="project.css" type="text/css" rel="stylesheet" />
+	</head>
+
+	<body>
+		<div id="main">
+			<div id="login">
+				<h1>Login</h1>
+					<!-- form goes here for login-->
+			</div>
+
+			<div id="page content">
+				<h1>Home Page</h1>
+				<p>Welcome to my CS 516 Project.  Feel free to look around!</p>
+			</div>
+		</div>
+
+		<div id="navigation">
+			<h1>Navigation</h1>
+			<ul>
+				<li><a href ="thichnhathanh.html">Thich Nhat Hanh</a></li>
+			</ul>
+		</div>
+	</body>
+</html>
