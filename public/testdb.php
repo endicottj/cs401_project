a7a134858c9e08df447d2b58fbd11396c8990536
Joey Endicott <endicottj@weiserschools.org>
2019-06-19 12:44:01 -0600

    Modified

--- public/testdb.php
+++ public/testdb.php
@@ -1,6 +1,8 @@
 <?php
-	require_once('includes/Dao.php');
-	$dao = new Dao();
-    $users = $dao->getUsers();
+    require_once('Dao.php');
     
+	$dao = new Dao();
+    
+    // $user = $doa->getUserByEmail('snoopy@peanuts.com');
+    // var_dump($user);
 ?>
